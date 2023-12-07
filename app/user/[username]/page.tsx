4415b4dc9b52d6c62b2feded606eafc1ea43796b
Grant Findlay <dev@grantfindlay.com>
2023-12-07 14:07:28 +0000

    fix layout

--- app/user/[username]/page.tsx
+++ app/user/[username]/page.tsx
@@ -28,11 +28,13 @@ export default async function User({
       <h1 className="text-3xl font-bold underline my-4">{user?.username}</h1>
       <h2>Address: {user?.Assets[0]?.address}</h2>
       <h2>Balance: {user?.Assets[0]?.balance}</h2>
+
+      <h3 className="mt-6 font-bold">Transactions</h3>
       {user?.Transactions?.map((tx) => {
         const event = JSON.parse((tx.eventData ?? "{}") as string) as any;
 
         return (
-          <div key={tx.id}>
+          <div key={tx.id} className="my-2">
             <p>Created: {event?.createdAt}</p>
             <p>Amount: {event?.amount}</p>
             <p>Status: {event?.status}</p>
